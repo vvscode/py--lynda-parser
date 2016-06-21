44b94b1927b961c20cfee7f602a30fc9995c8a67
Vasil Vanchuk <v.vanchuk@tut.by>
2016-06-21 21:32:29 +0300

    def main

---
+++ lynda-parser.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+# compatibility modules
+# from __future__ import unicode_literals
+# from __future__ import print_function
+# from builtins import str
+# from builtins import input
+
+# standard modules
+# import os
+# import re
+import sys
+# import getpass
+# import argparse
+# import errno
+# from io import open
+
+# external modules
+# import requests
+# from slugify import slugify
+# from bs4 import BeautifulSoup
+
+
+def main():
+    """ Main parser function"""
+    print('Parser main')
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
