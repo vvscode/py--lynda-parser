772fc970c479829c5f622e0356d405775bdd782b
Vasil Vanchuk <v.vanchuk@tut.by>
2016-07-12 21:06:04 +0300

    set_progress

--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,7 @@
 """" Helper function """
 import os
 import errno
+import sys
 
 
 def mkdir(directory):
@@ -12,3 +13,18 @@ def mkdir(directory):
         except OSError as exc:
             if exc.errno != errno.EEXIST:
                 raise exc
+
+
+def set_progress(progress, description):
+    if progress < 0:
+        progress = 0
+
+    if progress >= 1:
+        progress = 1
+
+    bar_length = 30
+    block_length = int(round(bar_length * progress))
+    text = "\r{0} [{1}] {2}%".format(description, ("#" * block_length) + (" " * (bar_length - block_length)), add_zero(int(progress * 100), 3))
+
+    sys.stdout.write(text)
+    sys.stdout.flush()
